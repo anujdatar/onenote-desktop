13c319a00802c54bd32f31244caf32ce6e196a0f
Anuj Datar <anuj.datar@gmail.com>
2019-03-29 02:33:23 -0400

    basic code done, lots of errors exist

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
+const configstore = require('configstore')
+
+const pkg = require('./package.json')
+const conf = new configstore(pkg.name, {
+  "windowBounds": {
+    "x": 100,
+    "y": 100,
+    "widht": 800,
+    "height": 600
+  }
+})
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -10,13 +22,18 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false,
+    icon: path.join(__dirname, '/src/images/logo.png'),
+    preload: path.join(__dirname, '/src/js/preloadInit.js'),
+    title: 'OneNote',
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: false
     }
   })
 
+  mainWindow.setBounds(conf.get('windowBounds'))
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('./src/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -50,3 +67,8 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+ipcMain.on('appClose', function() {
+  conf.set('windowBounds', mainWindow.getBounds())
+  mainWindow.close()
+})
