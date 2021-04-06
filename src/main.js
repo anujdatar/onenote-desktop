3b7922e0b1ca0e5c6578db75c31106b3d24ee72c
Anuj Datar <anuj.datar@gmail.com>
2021-04-06 18:15:41 +0530

    Edited comments for brevity

--- src/main.js
+++ src/main.js
@@ -64,22 +64,19 @@ function createWindow () {
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
+// create browser window when app ready
 app.on('ready', function () {
   createWindow()
 })
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
+  // handle macOS close and quit functionality
   if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
+  // create new window only if none exist
   if (mainWindow === null) createWindow()
 })
 
